9b775e1372bd3aa6e9e631eebe7f51618f39c058
sery <sery@533bbb3b-ce99-0410-9130-e5b668893301>
2011-01-28 15:02:59 +0000

    Changed interface of the interpolating_solvert. The solvers no longer needs to know about partition nesting beforehand. This information is tracked by partitioning_target_equationt.
    git-svn-id: https://scm.ti-edu.ch/reposvn/verify_cprover@12 533bbb3b-ce99-0410-9130-e5b668893301

--- trunk/cprover/src/funfrog/interpolation/opensmt_solver.cpp
+++ trunk/cprover/src/funfrog/interpolation/opensmt_solver.cpp
@@ -8,20 +8,14 @@ Author: Ondrej Sery
 
 #include "opensmt_solver.h"
 
-int
-opensmt_solvert::push_partition(/* TODO: partition id */)
-{
-  throw std::string(__func__) + " not yet implemented.";
-}
-
-void
-opensmt_solvert::pop_partition()
+fle_part_idt
+opensmt_solvert::new_partition()
 {
   throw std::string(__func__) + " not yet implemented.";
 }
 
 exprt
-opensmt_solvert::get_interpolant(int partition_id) const
+opensmt_solvert::get_interpolant(const fle_part_idst& partition_ids) const
 {
   throw std::string(__func__) + " not yet implemented.";
 }
