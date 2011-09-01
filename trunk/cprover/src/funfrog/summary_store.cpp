18b9b87c2deac6f9f0b56db596b236b817695c41
sery <sery@533bbb3b-ce99-0410-9130-e5b668893301>
2011-09-01 13:31:11 +0000

    Forgotten svn add
    git-svn-id: https://scm.ti-edu.ch/reposvn/verify_cprover@111 533bbb3b-ce99-0410-9130-e5b668893301

---
+++ trunk/cprover/src/funfrog/summary_store.cpp
@@ -0,0 +1,284 @@
+/*******************************************************************\
+
+Module: Storage class for function summaries (union-find).
+
+Author: Ondrej Sery
+
+\*******************************************************************/
+
+#include <string.h>
+
+#include "summary_store.h"
+#include "summary_info.h"
+
+summary_storet::nodet& summary_storet::find_repr(summary_idt id)
+{
+  nodet& node = store[id];
+  
+  if (node.is_repr()) {
+    return node;
+  }
+
+  std::cerr << "XXX Looking for summary " << id << std::endl;
+  
+  assert(node.repr_id != id);
+  
+  summary_storet::nodet& repr_node = find_repr(node.repr_id);
+  node.update_repr(repr_node.repr_id);
+  
+  return repr_node;
+}
+
+/*******************************************************************\
+
+Function: summary_storet::replace_summary
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: An already stored summary is implied by the new one - it is released
+ and represented by the stronger one, the id is still valid but represented
+ by the new one.
+
+\*******************************************************************/
+
+void summary_storet::replace_summary(summary_idt old_summary_id, 
+        summary_idt replacement_id)
+{
+  nodet& node = store[old_summary_id];
+  
+  assert(old_summary_id != replacement_id);
+  assert(node.is_repr());
+  assert(find_repr(replacement_id).repr_id != old_summary_id);
+  
+  node.update_repr(replacement_id);
+
+  std::cerr << "XXX Replaced summary " << old_summary_id << " by " <<
+          replacement_id << std::endl;
+  
+  repr_count--;
+}
+
+/*******************************************************************\
+
+Function: summary_storet::insert_summary
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Inserts a new summary, the given summary is invalidated
+
+\*******************************************************************/
+
+summary_idt summary_storet::insert_summary(summaryt& summary)
+{
+  summary_idt id = max_id++;
+  store.push_back(nodet(id, summary));
+  repr_count++;
+  
+  std::cerr << "XXX Inserted summary " << id << std::endl;
+  
+  return id;
+}
+
+/*******************************************************************\
+
+Function: summary_storet::find_summary
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Finds the representative of the given summary
+
+\*******************************************************************/
+
+summaryt& summary_storet::find_summary(summary_idt new_id)
+{
+  nodet& node = find_repr(new_id);
+  
+  return *(node.summary);
+}
+
+
+void summary_storet::mark_used_summaries(summary_infot& summary_info, 
+        bool used_mask[]) 
+{
+  call_sitest call_sites = summary_info.get_call_sites();
+  
+  for (call_sitest::iterator it = call_sites.begin();
+          it != call_sites.end(); ++it)
+  {
+    const summariest& summaries = 
+      it->second.get_summary_info().get_used_summaries();
+  
+    // Collect the used summaries
+    if (it->second.get_precision() != HAVOC) {
+      for (summariest::const_iterator it = summaries.begin();
+              it != summaries.end(); ++it)
+      {
+        used_mask[find_repr(*it).repr_id] = true;
+      }
+    }
+    
+    // Propagate the call
+    if (it->second.get_precision() == INLINE) {
+      mark_used_summaries(it->second.get_summary_info(), used_mask);
+    }
+  }
+}
+
+void summary_storet::remap_used_summaries(summary_infot& summary_info, 
+        summary_idt remap[]) 
+{
+  call_sitest call_sites = summary_info.get_call_sites();
+  
+  for (call_sitest::iterator it = call_sites.begin();
+          it != call_sites.end(); ++it)
+  {
+    summariest& summaries = 
+      it->second.get_summary_info().get_used_summaries();
+  
+    // Collect the used summaries
+    if (it->second.get_precision() != HAVOC) {
+      for (summariest::iterator it = summaries.begin();
+              it != summaries.end(); ++it)
+      {
+        *it = remap[*it];
+      }
+    }
+    
+    // Propagate the call
+    if (it->second.get_precision() == INLINE) {
+      remap_used_summaries(it->second.get_summary_info(), remap);
+    }
+  }
+}
+
+/*******************************************************************\
+
+Function: summary_storet::compact_store
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Compacts the store representation, only representatives are kept
+
+\*******************************************************************/
+
+void summary_storet::compact_store(summary_infot& summary_info, 
+        function_infost& function_infos)
+{
+  // FIXME: used_summaries is not filled anywhere :-)
+  
+  // Mask unused representatives
+  bool used_mask[max_id];
+  memset(&used_mask, 0, sizeof(used_mask));
+  
+  mark_used_summaries(summary_info, used_mask);
+  for (function_infost::iterator it = function_infos.begin();
+          it != function_infos.end(); ++it) {
+    const summariest& summaries = it->second.get_summaries();
+    
+    for (summariest::const_iterator it2 = summaries.begin();
+            it2 != summaries.end(); ++it2)
+    {
+      used_mask[*it2] = true;
+    }
+  }
+  
+  // Fill remap for the representatives
+  summary_idt remap[max_id];
+  summary_idt new_id = 0;
+  
+  for (summary_idt i = 0; i < max_id; ++i) {
+    if (store[i].is_repr() && used_mask[i]) {
+      remap[i] = new_id++;
+    }
+  }
+  
+  // Fill remap for the rest
+  for (summary_idt i = 0; i < max_id; ++i) {
+    remap[i] = remap[store[i].repr_id];
+  }
+  
+  // Remap and shrink
+  storet new_store;
+  
+  new_store.reserve(new_id);
+  
+  for (summary_idt i = 0; i < max_id; ++i) {
+    if (store[i].is_repr() && used_mask[i]) {
+      new_store.push_back(store[i]);
+    }
+  }
+
+  remap_used_summaries(summary_info, remap);
+  for (function_infost::iterator it = function_infos.begin();
+          it != function_infos.end(); ++it) {
+    summariest summaries;
+    it->second.set_summaries(summaries);
+    
+    for (summariest::iterator it2 = summaries.begin();
+            it2 != summaries.end(); ++it2)
+    {
+      *it2 = remap[*it2];
+    }
+    it->second.set_summaries(summaries);
+  }
+  
+  store.swap(new_store);
+  max_id = new_id;
+  repr_count = new_id;
+  
+  return;
+}
+
+// Serialization
+void summary_storet::serialize(std::ostream& out) const
+{
+  out << max_id << std::endl;
+
+  for (storet::const_iterator it = store.begin();
+          it != store.end();
+          ++it) {
+
+    out << it->repr_id << " " << it->is_repr() << std::endl;
+    
+    if (it->is_repr()) {
+      it->summary->serialize(out);
+    }
+  }
+}
+
+void summary_storet::deserialize(std::istream& in)
+{
+  repr_count = 0;
+  in >> max_id;
+
+  if (in.fail())
+    return;
+
+  store.clear();
+  store.reserve(max_id);
+  
+  for (unsigned i = 0; i < max_id; ++i)
+  {
+    summary_idt repr_id;
+    bool is_repr;
+    summaryt summary;
+    
+    in >> repr_id >> is_repr;
+    
+    if (is_repr) {
+      summary.deserialize(in);
+      store.push_back(nodet(repr_id, summary));
+      repr_count++;
+    } else {
+      store.push_back(nodet(repr_id));
+    }
+  }
+}
---
+++ trunk/cprover/src/funfrog/summary_store.h
@@ -0,0 +1,96 @@
+/*******************************************************************\
+
+Module: Storage class for function summaries (union-find).
+
+Author: Ondrej Sery
+
+\*******************************************************************/
+
+#ifndef CPROVER_SUMMARY_STORE_H
+#define CPROVER_SUMMARY_STORE_H
+
+#include <ostream>
+#include "solvers/prop_itp.h"
+
+typedef prop_itpt summaryt;
+typedef long unsigned summary_idt;
+typedef std::vector<summary_idt> summariest;
+class summary_infot;
+class function_infot;
+typedef hash_map_cont<irep_idt, function_infot, irep_id_hash> function_infost;
+
+class summary_storet
+{
+public:
+  summary_storet() : max_id (0), repr_count(0) {}
+
+  // Serialization
+  void serialize(std::ostream& out) const;
+  void deserialize(std::istream& in);
+
+  // Compacts the store representation, only representatives are kept.
+  void compact_store(summary_infot& summary_info, 
+          function_infost& function_infos);
+  
+  // An already stored summary is implied by the new one - it is released
+  // and represented by the stronger one, the id is still valid but represented
+  // by the new one.
+  void replace_summary(summary_idt old_summary_id, summary_idt replacement_id);
+  // Inserts a new summary, the given summary is invalidated
+  summary_idt insert_summary(summaryt& summary);
+  // Finds the representative of the given summary
+  summaryt& find_summary(summary_idt new_id);
+  
+  // Reset the summary store
+  void clear() { store.clear(); max_id = 0; repr_count = 0; }
+
+private:
+
+  // Union find node
+  struct nodet {
+    // Note that the given summary is destroyed
+    nodet(summary_idt id, summaryt& _summary) : repr_id(id) {
+      summary = new summaryt();
+      summary->swap(_summary);
+    }
+    
+    nodet(summary_idt _repr_id) : summary(NULL), repr_id(_repr_id)  { }
+    
+    nodet(const nodet& other) : summary(other.summary), repr_id(other.repr_id) {
+      const_cast<nodet&>(other).summary = NULL;
+    }
+    
+    nodet() : summary(NULL), repr_id(0) {}
+    
+    ~nodet() { if (summary != NULL) delete summary; }
+    
+    bool is_repr() const { return summary != NULL; }
+    
+    void update_repr(summary_idt _repr_id) {
+      if (is_repr()) {
+        delete summary;
+        summary = NULL;
+      }
+      repr_id = _repr_id;
+    }
+    
+    // The summary itself
+    summaryt* summary;
+    // Keeps id of the representative (if the node is representative, than this
+    // means its own id)
+    summary_idt repr_id;
+  };
+  
+  nodet& find_repr(summary_idt id);
+  void mark_used_summaries(summary_infot& summary_info, bool used_mask[]);
+  void remap_used_summaries(summary_infot& summary_info, summary_idt remap[]);
+  
+  // Maximal used id
+  summary_idt max_id;
+  summary_idt repr_count;
+          
+  typedef std::vector<nodet> storet;
+  storet store;
+};
+
+#endif
