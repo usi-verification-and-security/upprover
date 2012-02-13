96497ac7f99d22dcc9e1e911fae5348e491c3d25
fedyukovich <fedyukovich@533bbb3b-ce99-0410-9130-e5b668893301>
2012-02-13 10:40:19 +0000

    output of the goto-instructions count
    git-svn-id: https://scm.ti-edu.ch/reposvn/verify_cprover@202 533bbb3b-ce99-0410-9130-e5b668893301

--- trunk/cprover/src/funfrog/subst_scenario.cpp
+++ trunk/cprover/src/funfrog/subst_scenario.cpp
@@ -207,7 +207,7 @@ void subst_scenariot::setup_last_assertion_loc(const assertion_infot& assertion)
       last_assertion_loc = r.second;
     }
   }
-  std::cout << "Last assertion location: " << last_assertion_loc << std::endl;
+  std::cout << "Last assertion location: " << last_assertion_loc << " / " << global_loc << std::endl;
 
   single_assertion_check = count == 1;
 
