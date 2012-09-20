cd9a8d5a27c231667715c27e14923b84b2c9f12e
sery <sery@533bbb3b-ce99-0410-9130-e5b668893301>
2012-09-20 12:10:23 +0000

    Error trace reordered (regression tests don't go through --> untested).
    git-svn-id: https://scm.ti-edu.ch/reposvn/verify_cprover@228 533bbb3b-ce99-0410-9130-e5b668893301

--- trunk/cprover/src/funfrog/prop_assertion_sum.cpp
+++ trunk/cprover/src/funfrog/prop_assertion_sum.cpp
@@ -153,12 +153,101 @@ bool prop_assertion_sumt::is_satisfiable(
   }
 }
 
+// Copied from build_goto_tarce.cpp
+void build_exec_order_goto_trace(
+  partitioning_target_equationt &target,
+  const prop_convt &prop_conv,
+  goto_tracet &goto_trace)
+{
+  unsigned step_nr=0;
+  
+  const SSA_steps_orderingt& SSA_steps = target.get_steps_exec_order();
+  
+  for(SSA_steps_orderingt::const_iterator
+      it=SSA_steps.begin();
+      it!=SSA_steps.end();
+      it++)
+  {
+    const symex_target_equationt::SSA_stept &SSA_step=**it;
+    
+    if(prop_conv.prop.l_get(SSA_step.guard_literal)!=tvt(true))
+      continue;
+
+    if(SSA_step.is_assignment() &&
+       SSA_step.assignment_type==symex_target_equationt::HIDDEN)
+      continue;
+
+    step_nr++;
+    
+    goto_trace.steps.push_back(goto_trace_stept());    
+    goto_trace_stept &goto_trace_step=goto_trace.steps.back();
+    
+    goto_trace_step.thread_nr=SSA_step.source.thread_nr;
+    goto_trace_step.lhs=SSA_step.lhs;
+    goto_trace_step.rhs=SSA_step.rhs;
+    goto_trace_step.pc=SSA_step.source.pc;
+    goto_trace_step.comment=SSA_step.comment;
+    goto_trace_step.original_lhs=SSA_step.original_lhs;
+    goto_trace_step.type=SSA_step.type;
+    goto_trace_step.step_nr=step_nr;
+    goto_trace_step.format_string=SSA_step.format_string;
+    goto_trace_step.io_id=SSA_step.io_id;
+    goto_trace_step.formatted=SSA_step.formatted;
+    
+    if(SSA_step.lhs.is_not_nil())
+      goto_trace_step.value=prop_conv.get(SSA_step.lhs);
+    
+    for(std::list<exprt>::const_iterator
+        j=SSA_step.converted_io_args.begin();
+        j!=SSA_step.converted_io_args.end();
+        j++)
+    {
+      const exprt &arg=*j;
+      if(arg.is_constant() ||
+         arg.id()==ID_string_constant)
+        goto_trace_step.io_args.push_back(arg);
+      else
+      {
+        exprt tmp=prop_conv.get(arg);
+        goto_trace_step.io_args.push_back(tmp);
+      }
+    }
+
+    if(SSA_step.is_assert() ||
+       SSA_step.is_assume())
+    {
+      goto_trace_step.cond_expr=SSA_step.cond_expr;
+
+      goto_trace_step.cond_value=
+        prop_conv.prop.l_get(SSA_step.cond_literal).is_true();
+    }
+
+    if(SSA_step.is_assert())
+    {
+      // we stop after a violated assertion
+      if(!goto_trace_step.cond_value)
+        break;
+    }
+    else if(SSA_step.is_assume())
+    {
+      // assumptions can't be false
+      // This is not necessarily true for partitioned_target_equation
+      //assert(goto_trace_step.cond_value);
+    }
+  }
+}
+
 void prop_assertion_sumt::error_trace(const prop_convt &prop_conv, const namespacet &ns)
 {
   status("Building error trace");
 
   goto_tracet goto_trace;
-  build_goto_trace(equation, prop_conv, goto_trace);
+  
+  // Original trace builder:
+  // build_goto_trace(equation, prop_conv, goto_trace);
+  
+  // New exec order trace builder;
+  build_exec_order_goto_trace(equation, prop_conv, goto_trace);
 
   #if 0
   if(options.get_option("vcd")!="")
