5b3446e29b78e3982adb4162e7dc716e13bd528b
3007882@usi.ch <3007882@usi.ch@533bbb3b-ce99-0410-9130-e5b668893301>
2013-03-14 01:52:41 +0000

    out of bounds fix
    git-svn-id: https://scm.ti-edu.ch/reposvn/verify_cprover@300 533bbb3b-ce99-0410-9130-e5b668893301

--- trunk/cprover/src/funfrog/solvers/satcheck_opensmt.cpp
+++ trunk/cprover/src/funfrog/solvers/satcheck_opensmt.cpp
@@ -720,7 +720,9 @@ void satcheck_opensmtt::addColors(const std::vector<unsigned>& symbols,
 void satcheck_opensmtt::addBitBlastBinding(boolbv_mapt::literal_mapt& map){
   std::vector<Enode*> v;
   for (unsigned i = 0; i < map.size(); i++){
-    v.push_back(enodes[map[i].l.var_no()]);
+    if(map[i].l.var_no() < enodes.size()){
+      v.push_back(enodes[map[i].l.var_no()]);
+    }
   }
   opensmt_ctx->addBitBlastBinding(v);
 }
