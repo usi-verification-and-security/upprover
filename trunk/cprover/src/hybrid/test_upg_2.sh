86eb0f0ae15be763669e9c6dbf03c40ac5009f2d
3011590@usi.ch <3011590@usi.ch@533bbb3b-ce99-0410-9130-e5b668893301>
2013-06-25 07:35:45 +0000

    Paths right to allow running from arbitrary directories
    git-svn-id: https://scm.ti-edu.ch/reposvn/verify_cprover@326 533bbb3b-ce99-0410-9130-e5b668893301

--- trunk/cprover/src/hybrid/test_upg_2.sh
+++ trunk/cprover/src/hybrid/test_upg_2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+checker=/u1/home/aehyvari/src/funfrog/trunk/cprover/src/hybrid/naive-hybrid-check-2.py
 
 if [[ $# != 5 ]]; then
     echo "Usage: $0 <example> <old> <new> <name> <yes|no>               "
@@ -9,9 +10,9 @@ if [[ $# != 5 ]]; then
     echo "                  version and assertions                      "
     echo "   <new>          is the directory containing the new version "
     echo "                  and its assertions                          "
-    echo "   <yes|no>       yes if use cbmc, no if use evolcheck        "
-    echo "   <filen>        the name of the c file (same for both       "
+    echo "   <name>         the name of the c file (same for both       "
     echo "                  versions)                                   "
+    echo "   <yes|no>       yes if use cbmc, no if use evolcheck        "
     exit 1
 fi
 
@@ -21,7 +22,6 @@ v1=$1; shift
 name=$1; shift
 cbmc=$1; shift
 
-cwd=`pwd`
 cd $example
 rm result/__* results/a.out
 
@@ -29,7 +29,7 @@ rm __*
 echo "################################################################################"
 
 #python ../naive-hybrid-check.py --initial-check tcas_v0/init_ass.txt tcas_v0 result tcas.c
-python ../naive-hybrid-check-2.py --initial-check ${cwd}/${v0}/init_ass.txt ${cwd}/${v0} result ${cbmc} ${name}
+python ${checker} --initial-check ${v0}/init_ass.txt ${v0} result ${cbmc} ${name}
 if [[ $? != 0 ]]; then exit; fi
 cp result/a.out __old_a.out
 
@@ -37,6 +37,6 @@ cp result/a.out __old_a.out
 echo "################################################################################"
 
 #python ../naive-hybrid-check.py --upgrade-check __old_a.out __trusted tcas_v0/new_ass.txt tcas_v0 result tcas.c
-python ../naive-hybrid-check-2.py --upgrade-check __old_a.out __trusted ${cwd}/${v1}/new_ass.txt ${cwd}/${v1} result ${cbmc} ${name}
+python ${checker} --upgrade-check __old_a.out __trusted ${v1}/new_ass.txt ${v1} result ${cbmc} ${name}
 
 
