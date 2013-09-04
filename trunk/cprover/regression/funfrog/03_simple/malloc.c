2c4e06f3861f1d3d78ab2b9264352997f1b61a2a
3007882@usi.ch <3007882@usi.ch@533bbb3b-ce99-0410-9130-e5b668893301>
2013-09-04 15:54:32 +0000

    simple dynamic memory allocation tests
    git-svn-id: https://scm.ti-edu.ch/reposvn/verify_cprover@336 533bbb3b-ce99-0410-9130-e5b668893301

---
+++ trunk/cprover/regression/funfrog/03_simple/malloc.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+int nondet_int();
+
+int get_int (){
+  int a = nondet_int();
+  __CPROVER_assume(a > 7);
+  __CPROVER_assume(a < 10);
+  return a;
+}
+
+
+void main(){
+
+  int b = get_int();
+
+  int * arr = malloc (b);
+
+
+  if (arr == NULL)
+	return;
+
+
+  for (int i = 0; i < b; i++){
+    arr[i] = i;
+  }
+
+  assert(arr[1] == 0);
+
+}
