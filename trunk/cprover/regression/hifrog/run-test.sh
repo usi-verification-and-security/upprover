a13ea56eefea0a81d7622f062b8239060467ab3a
Karine Even <karine.even_mendoza@kcl.ac.uk>
2017-04-06 09:05:57 +0100

    basic running script

--- trunk/cprover/regression/hifrog/run-test.sh
+++ trunk/cprover/regression/hifrog/run-test.sh
@@ -4,6 +4,29 @@ echo " - date: $(date '+%Y-%m-%d at %H:%M.%S')"
 echo " - host name $(hostname -f)"
 echo " - script path: $(readlink -f $0)"
 
-hifrog=../../cprover/src/funfrog/hifrog
+hifrog=./../../src/funfrog/hifrog
 OUTPUT_DIR="output_tmp"
+
+# Iterating over all the test cases - When result shall match the known results
+for filename in testcases/*.c_tc 
+do
+    # Per file, run all its inner test cases. 0 => clear summaries
+    echo "Run test cases file: $filename" 
+    IFS=$'\n'
+    for next in `cat $filename`
+    do
+	chrlen=${#next}
+	if (($chrlen == 1))
+	then
+		echo "rm __summaries"
+      		rm __summaries
+	else
+		echo $hifrog $next
+                temp="${next%\"}"
+		temp="${temp#\"}"
+		arr=(`echo $temp | sed 's/,/\n/g'`)
+		$hifrog ${arr[0]} --logic ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]} #stupid way to do it, but works
+   	fi
+    done
+done
 
