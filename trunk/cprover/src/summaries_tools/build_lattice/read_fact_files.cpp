e1e9f35d641228f093f5a36060c79b1dcffd012d
Karine Even <karine.even_mendoza@kcl.ac.uk>
2017-09-25 11:50:02 +0100

    util for extracting all subsets of facts for co-exist check

--- trunk/cprover/src/summaries_tools/build_lattice/read_fact_files.cpp
+++ trunk/cprover/src/summaries_tools/build_lattice/read_fact_files.cpp
@@ -6,7 +6,10 @@
  */
 
 #include "read_fact_files.h"
+
+#include <iostream>
 #include <fstream>
+using namespace std;
 
 #include "../../funfrog/hifrog.h"
 
@@ -26,32 +29,29 @@
 bool read_fact_filest::load_facts(string facts_decl_file_name, string facts_file_name) 
 {
     /* Reads Declarations of SMT file */
-    std::ifstream input(facts_decl_file_name);
-    if (!input.is_open()) 
+    std::ifstream input_decl(facts_decl_file_name);
+    if (!input_decl.is_open())
     {
         std::cout << "Cannot find the facts's declarations file: " << facts_decl_file_name << std::endl;
         return false;
     }
     
     std::string line;
-    while(std::getline(input, line)) 
+    while(std::getline(input_decl, line))
     {
         decls.insert(line);
     }
     
     /* Reads the facts */
-    std::ifstream input(facts_file_name);
-    if (!input.is_open()) 
+    std::ifstream input_facts(facts_file_name);
+    if (!input_facts.is_open())
     {
         std::cout << "Cannot open the facts' file: " <<  facts_file_name << std::endl;
         return false;
     }
-
-        // Read the file
-    string line = "";
     
     // Name of the original function. E.g., mod
-    if (!std::getline(input, line)) {
+    if (!std::getline(input_facts, line)) {
         std::cout << "Error reading original_function_name \n";
         return false;
     }
@@ -59,28 +59,28 @@ bool read_fact_filest::load_facts(string facts_decl_file_name, string facts_file
     std::cout << "Start loading " << facts_file_name << " function's facts: " << original_function_name << std::endl;
     
     // Declare of the original function
-    if (!std::getline(input, line)) {
+    if (!std::getline(input_facts, line)) {
         std::cout << "Error reading declare_original_function \n";
         return false;
     }
     // Skip it, we don't need if for this task
     
     // Define part of the original function header
-    if (!std::getline(input, line)) {
+    if (!std::getline(input_facts, line)) {
         std::cout << "Error reading original_header_function \n";
         return false;
     }
     original_header_function = line;
     
     // Define part of the original function header
-    if (!std::getline(input, line)) {
+    if (!std::getline(input_facts, line)) {
         std::cout << "Error reading is_return_unsigned \n";
         return false;
     }
     // Skip it, we don't need if for this task
     
     // in/out parameters of the facts
-    if (!std::getline(input, line)) {
+    if (!std::getline(input_facts, line)) {
         std::cout << "Error reading params_set_of_function \n";
         return false;
     }
@@ -90,11 +90,11 @@ bool read_fact_filest::load_facts(string facts_decl_file_name, string facts_file
     std::string name="";
     std::string fact="";
     std::cout << "==> Reading facts: ";
-    while (std::getline(input, name)) {
+    while (std::getline(input_facts, name)) {
         if (name[0] == ';') continue;
         
         std::cout << name << " ";
-        if (!std::getline(input, fact)) {
+        if (!std::getline(input_facts, fact)) {
             return false;
         }
 
@@ -116,6 +116,7 @@ bool read_fact_filest::load_facts(string facts_decl_file_name, string facts_file
  Outputs: sub-set of facts in smt-lib query
 
  Purpose: main output of the smt queries for building the lattice
+ ==> Input for the co-exist check
  
 \*******************************************************************/
 void read_fact_filest::save_facts_smt_queries(string facts_query_base_file_name)
@@ -125,36 +126,96 @@ void read_fact_filest::save_facts_smt_queries(string facts_query_base_file_name)
     for ( auto it = decls.begin(); it != decls.end(); it++ )
         smt_decl += (*it) + " \n";
     
-     // Add all the facts to the file
-    for (auto i = facts.begin(); i != facts.end(); ++i) 
+    // Create all the subsets of the current set of facts
+    vector<pair<string,string>> set_facts;
+    for (auto i = facts.begin(); i != facts.end(); ++i)
+    	set_facts.push_back(pair<string, string> (i->first, i->second));
+
+    vector< vector<pair<string,string>> > subsets = create_all_subsets_of_facts(set_facts);
+
+    // Add all the subsets of facts to the file differnt files
+    for (auto i = subsets.begin(); i != subsets.end(); ++i)
     {
-        string fact_name = i->first;
-        string fact = i->second;
-        
-        // definition of the original function - connect to inner fact
-        string orig_func_call = create_local_call_to_orig_func(fact_name);
-        // Created: (= (|_mod#0| |mod_Cd::a!0| |mod_Cd::n!0|) mod_Cd::return_value!0)
-        
-        string orig_params_connection = create_params_args_connection(fact_name);
-        // Created: (and (= |_mod#0::a!0| |mod_Cd::a!0|) (= |_mod#0::n!0| |mod_Cd::n!0|))
-        
-        string outter_fact = "(and " + fact + " (and " + orig_func_call + " " + orig_params_connection "))";
-        
-        // write fact with only one fact:
-        write_smt_query(outter_fact, smt_decl, facts_query_base_file_name, fact_name, "01");
-        
-        for (auto j = i->begin(); j != i->end(); ++j) 
-        {
-        /* ... */
-        }
+    	// Build a query to a single subset of facts
+    	string query = "";
+    	for (auto j = i->begin(); j != i->end(); ++j)
+    	{
+    		string outter_fact = create_string_of_single_fact(j->first, j->second);
+    		query += outter_fact + "\n";
+    	}
+
+    	// If not an empty set
+    	if (i->size() > 0)
+    	{
+    		string fact_name = i->begin()->first;
+    		string counter = std::to_string(i->size());
+
+    		if (i->size() > 1)
+    		{
+    			query = "(and " + query + ")\n";
+    		}
+    		query = "(assert \n" + query + "\n)\n(check-sat)\n";
+
+    		// write fact with only one fact:
+    		write_smt_query(query, smt_decl, facts_query_base_file_name, fact_name, counter);
+    		std::cout << "Writing Query " << fact_name << "::" << counter << " to file." << std::endl;
+    	}
     }
 }
 
-void read_fact_filest::write_smt_query(
-    string facts_str, string decls_str, string base_name, 
-        string start_fact_name, string counter)
+/*******************************************************************
+
+ Function: read_fact_filest::create_all_subsets_of_facts
+
+ Inputs: facts
+
+ Outputs: sub-set of facts in vectors
+
+ Purpose:
+
+\*******************************************************************/
+vector< vector<pair<string,string>> > read_fact_filest::create_all_subsets_of_facts(vector<pair<string,string>> set_facts)
 {
-    
+    vector< vector<pair<string,string>> > subset;
+    vector<pair<string,string>> empty;
+    subset.push_back( empty );
+
+    for (unsigned i = 0; i < set_facts.size(); i++)
+    {
+        vector<vector<pair<string,string>>> subsetTemp = subset;
+
+        for (unsigned j = 0; j < subsetTemp.size(); j++)
+            subsetTemp[j].push_back(set_facts[i]);
+
+        for (unsigned j = 0; j < subsetTemp.size(); j++)
+            subset.push_back(subsetTemp[j]);
+    }
+    return subset;
+}
+
+/*******************************************************************
+
+ Function: read_fact_filest::create_string_of_single_fact
+
+ Inputs: a fact and its name
+
+ Outputs: the fact as a string connected to the main arguments
+
+ Purpose:
+
+\*******************************************************************/
+string read_fact_filest::create_string_of_single_fact(string fact_name, string fact)
+{
+    // definition of the original function - connect to inner fact
+    string orig_func_call = create_local_call_to_orig_func(fact_name);
+    // Created: (= (|_mod#0| |mod_Cd::a!0| |mod_Cd::n!0|) mod_Cd::return_value!0)
+
+    string orig_params_connection = create_params_args_connection(fact_name);
+    // Created: (and (= |_mod#0::a!0| |mod_Cd::a!0|) (= |_mod#0::n!0| |mod_Cd::n!0|))
+
+    string outter_fact = "(and " + fact + " (and " + orig_func_call + " " + orig_params_connection + "))";
+
+    return outter_fact;
 }
 
 /*******************************************************************
@@ -173,7 +234,7 @@ string read_fact_filest::create_local_call_to_orig_func(string fact_name) {
     string return_val = "|" + fact_name + FUNC_RETURN + "|" ;
     std::string params = original_header_function;
     std::string func_name = original_function_name;
-    index = 0;
+    unsigned index = 0;
     while ((index = params.find(func_name, index)) != std::string::npos) {
         params = params.replace(index, func_name.size(),fact_name);
         index += fact_name.size();
@@ -201,7 +262,7 @@ string read_fact_filest::create_params_args_connection(string fact_name)
     std::string args = original_header_function;
     std::string params = original_header_function;
     std::string func_name = original_function_name;
-    index = 0;
+    unsigned index = 0;
     while ((index = params.find(func_name, index)) != std::string::npos) {
         params = params.replace(index, func_name.size(),fact_name);
         index += fact_name.size();
@@ -241,7 +302,7 @@ string read_fact_filest::create_params_args_connection(string fact_name)
  Purpose: split a string according to split_str token
  
 \*******************************************************************/
-void factory_lattice_refiner_modelt::split(std::list<std::string>& strings, std::string list, std::string split_str)
+void read_fact_filest::split(std::list<std::string>& strings, std::string list, std::string split_str)
 {
   int length=list.length();
 
@@ -253,4 +314,30 @@ void factory_lattice_refiner_modelt::split(std::list<std::string>& strings, std:
     if(next==std::string::npos) break;
     idx=next;
   }
+}
+
+/*******************************************************************
+
+ Function: read_fact_filest::write_smt_query
+
+ Inputs: facts as a string + header of the smt file
+
+ Outputs: smt file
+
+ Purpose:
+
+\*******************************************************************/
+void read_fact_filest::write_smt_query(
+    string facts_str, string decls_str, string base_name,
+        string start_fact_name, string counter)
+{
+    // Write to a file: (test_path + file_no)
+    ofstream smtfile;
+    smtfile.open (base_name + "_" + start_fact_name + "_" + counter + "__" + std::to_string(file_no) + ".smt2");
+    smtfile << decls_str << "\n";
+    smtfile << facts_str;
+    smtfile.close();
+
+    // Inc the counter file_no
+    file_no++;
 }
--- trunk/cprover/src/summaries_tools/build_lattice/read_fact_files.h
+++ trunk/cprover/src/summaries_tools/build_lattice/read_fact_files.h
@@ -9,12 +9,17 @@
 #define READ_FACT_FILES_H
 
 #include <iostream>
+#include <list>
+#include <map>
+#include <vector>
+#include <set>
+//#include <algorithm>
 
 using namespace std;
 
 class read_fact_filest {
 public:
-    read_fact_filest() {}
+    read_fact_filest() :file_no(0) {}
     virtual ~read_fact_filest() {}
     
     bool load_facts(string facts_decl_file_name, string facts_file_name);
@@ -23,14 +28,19 @@ public:
 private:
     std::set<std::string> decls;
     std::map<std::string,std::string> facts;
+    int file_no; // When writting to avoid duplicate names
     
     string original_header_function;
     string original_function_name;
     
     string create_local_call_to_orig_func(string fact_name);
     string create_params_args_connection(string fact_name);
-    void split(std::list<std::string>& strings, std::string list, std::string split_str);
+    string create_string_of_single_fact(string fact_name, string fact);
+
+    vector<vector<pair<string,string>>> create_all_subsets_of_facts(vector<pair<string,string>> set_facts);
     void write_smt_query(string facts_str, string decls_str, string base_name, string start_fact_name, string counter);
+
+    void split(std::list<std::string>& strings, std::string list, std::string split_str);
 };
 
 #endif /* READ_FACT_FILES_H */
