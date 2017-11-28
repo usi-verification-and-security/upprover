dfc293eb29e0bcc593ac1f6b52d5d4801f68f56b
Karine Even <karine.even_mendoza@kcl.ac.uk>
2017-11-28 16:22:20 +0000

    add benchmarks for partial loop test

---
+++ trunk/cprover/regression/hifrog/benchmarks/loop_assert_13.c
@@ -0,0 +1,28 @@
+int a0 = 0;
+int calculate_output(_a0)
+{
+    if (_a0 == 0)
+        return 5;
+
+    return _a0;
+}
+
+void loop()
+{
+    while(1)
+    {
+        assert(a0 != 5);
+        a0 = calculate_output(a0);
+    }
+
+    assert(a0 == 5);
+}
+
+int main()
+{
+    int a0 = 0;
+
+    loop();
+
+    assert(a0 == 5);
+}
