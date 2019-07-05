f31fba368900c3e758e654a211ff81abf348ddf1
Sepideh Asadi <asadis@usi.ch>
2019-07-05 04:41:49 +0200

    add unit test to check whether the summary ID remains the same after serialize/deserialize; not working yet

--- trunk/cprover/src/funfrog/test/unit/test_SummaryStore.cpp
+++ trunk/cprover/src/funfrog/test/unit/test_SummaryStore.cpp
@@ -8,6 +8,10 @@
 #include <funfrog/prop_summary_store.h>
 #include <funfrog/solvers/smt_itp.h>
 #include <funfrog/solvers/prop_itp.h>
+#include <funfrog/solvers/solver_options.h>
+#include <funfrog/solvers/smtcheck_opensmt2_lra.h>
+#include <fstream>
+#include <string>
 
 TEST(test_Insert, test_Insert_Different){
     summary_storet* ss = new smt_summary_storet;
@@ -53,3 +57,51 @@ TEST(test_Insert, test_Insert_Different_Prop){
     auto res = ss->get_summaries(fun);
     ASSERT_EQ(res.size(), 2);
 }
+
+
+//In this test we check when a given summary is serialized to a FILE, and then in the second run
+// gets deserialized from the FILE, the summaryID remains the same.
+TEST(test_IDequality, test_IDequality_serialize_deserialize_lra) {
+
+    solver_optionst sop;
+    smtcheck_opensmt2t* decider1 = new smtcheck_opensmt2t_lra(sop, "lra checker");
+    smt_summary_storet * store1 = new smt_summary_storet(decider1);
+
+//    auto lra_solver_ptr = std::unique_ptr<smtcheck_opensmt2t_lra>{new smtcheck_opensmt2t_lra(solver_options, "lra checker")};
+//    smtcheck_opensmt2t_lra* lra_solver1 = new smtcheck_opensmt2t_lra(solver_options, "lra checker");  //raw
+//    auto & lra_solver = *lra_solver_ptr;
+
+    smt_itpt* itp1 = new smt_itpt;
+    itp1->setInterpolant(PTRef{1});
+    std::string fun{"foo"};
+    store1->insert_summary(itp1, fun);
+//  auto sum_ID1 = store1->get_summaries(fun)[0];
+
+    itp1->setDecider(decider1);   //? Is it needed?
+    const std::string& summary_file = "__summaries";  //file goes to build/bin/
+    if (!summary_file.empty()) {
+    std::ofstream out;
+    out.open(summary_file.c_str());
+    store1->serialize(out);    //hits assert in Alloc.h in OpenSMT
+    out.close();
+    }
+
+    delete store1;
+    delete decider1;
+
+//create again(simulates 2nd run)
+    smtcheck_opensmt2t* decider2 = new smtcheck_opensmt2t_lra(sop, "lra checker");
+    smt_summary_storet * store2 = new smt_summary_storet(decider2);
+
+    std::vector <std::string> summary_files;
+    summary_files.push_back(summary_file);
+
+    if (!summary_files.empty()) {
+        store2->deserialize(summary_files);
+    }
+    auto sum_ID2 = store2->get_summaries(fun)[0];
+//  check if  they are equal(sum_ID1, sum_ID2):
+    auto res = store2->find_summary(sum_ID2).equals(itp1);
+    ASSERT_TRUE(res);
+}
+
