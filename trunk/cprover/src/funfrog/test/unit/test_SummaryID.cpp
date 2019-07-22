485b8e9065d5fac04810537e8c1220281e3afce9
Sepideh Asadi <asadis@usi.ch>
2019-07-22 20:39:32 +0200

    add more unit tests to examine sumID preservation with real summary formulas; at this point 2 test fail

--- trunk/cprover/src/funfrog/test/unit/test_SummaryID.cpp
+++ trunk/cprover/src/funfrog/test/unit/test_SummaryID.cpp
@@ -14,9 +14,12 @@
 #include <funfrog/partition_iface.h>
 #include <funfrog/call_tree_node.h>
 
-//This test checks when a given summary is serialized to a FILE, and then in the second run
-// gets deserialized from the same FILE, the summaryID remains the same.
 
+/********************************************************************
+ This test checks when a given summary is serialized to a FILE,
+ and then in the second run gets deserialized from the same FILE,
+ the summaryID remains the same.
+********************************************************************/
 TEST(test_IDequality, test_IDequality_serialize_deserialize_lra) {
 
     solver_optionst sop;
@@ -55,17 +58,267 @@ TEST(test_IDequality, test_IDequality_serialize_deserialize_lra) {
     if (!summary_files.empty()) {
         store2->deserialize(summary_files);
     }
-
-//get the summaries from both stores based on a particular ID
+//use ID to get back the initial summary(we cannot use the summary pointer(here stub) we passed for storing the summary,
+// since summary store requires the ownership of summary)
     itpt& itp1 = store1->find_summary(sum_ID1);
+//get the summaries from both stores based on a particular ID
     itpt& itp2 = store2->find_summary(sum_ID1);
 
     auto res = itp2.equals(&itp1);
     ASSERT_TRUE(res);
+    if(res){
+        std::cout << "1st test successfully passed!\n" <<'\n';
+    }
     //deletes summary FILE from build/bin/
+    std::remove("__summaries");
+    delete decider2;
+}
+
+/********************************************************************
+ Test ID preservation for single artificial summary
+********************************************************************/
+TEST(test_IDequality, test_IDequality_serialize_deserialize_uf) {
+    
+    solver_optionst sop;
+    smtcheck_opensmt2t_uf* decider1 = new smtcheck_opensmt2t_uf(sop, "uf checker");
+    smt_summary_storet * store1 = new smt_summary_storet(decider1);
+    
+    std::string fun{"bar"};
+    summary_idt sum_ID1;
+    
+    {
+        smt_itpt *newitp1 = new smt_itpt();
+        //create cprover expression using symbol_exprt(name, type)
+        symbol_exprt x("x", typet(ID_integer));
+        constant_exprt zero(ID_0, typet(ID_integer));
+        // x >= 0
+        binary_relation_exprt comp(x, ID_ge, zero);
+//        get convertor from decider
+//        auto* convertor = decider1->get_convertor();
+//        FlaRef ref = convertor->convert_bool_expr(comp);
+//        (void) ref;
+        PTRef summary_temp1 = decider1->expression_to_ptref(comp);
+        //manually set the PTRef
+        newitp1->setInterpolant(summary_temp1);
+        newitp1->setDecider(decider1);
+        newitp1->getTempl().addArg(decider1->expression_to_ptref(x));
+        newitp1->getTempl().setBody(summary_temp1);
+        
+        sum_ID1 = store1->insert_summary(newitp1, fun);
+        // insert_summary passes the ownership to summary_store, don't use *new_itp anymore
+    }
+//serialization
+    const std::string& summary_file = "__summaries";  //file goes to build/bin/
+    std::ofstream out;
+    out.open(summary_file.c_str());
+    store1->serialize(out);
+    out.close();
+    delete decider1;
+
+//create resources again (simulates the 2nd run)
+    smtcheck_opensmt2t* decider2 = new smtcheck_opensmt2t_uf(sop, "uf checker");
+    smt_summary_storet * store2 = new smt_summary_storet(decider2);
+    
+    std::vector <std::string> summary_files;
+    summary_files.push_back(summary_file);
+    
+    if (!summary_files.empty()) {
+        store2->deserialize(summary_files);
+    }
+
+    itpt& itp1 = store1->find_summary(sum_ID1);
+    itpt& itp2 = store2->find_summary(sum_ID1);
+//check if both itps are equal for the same ID
+    auto res = itp2.equals(&itp1);
+    ASSERT_TRUE(res);
+    if(res){
+        std::cout << "2nd test successfully passed! \n" <<'\n';
+    }
+    delete decider2;
     std::remove("__summaries");
 }
 
-//alternative approach: for given func, ask the ID, then ask its ITP
-// summary_idt sum_ID2 = store2->get_summariesID(fun)[0];
-// itpt& itp2 = store2->find_summary(sum_ID2);
+/********************************************************************
+ Test sumID preservation for two artificial summaries in EUF
+********************************************************************/
+//two summaries for the same function
+TEST(test_IDequality, test_IDequality_serialize_deserialize_uf_twoSum) {
+    
+    solver_optionst sop;
+    smtcheck_opensmt2t_uf* decider1 = new smtcheck_opensmt2t_uf(sop, "uf checker");
+    smt_summary_storet * store1 = new smt_summary_storet(decider1);
+    
+    std::string fun{"alfa"};
+    
+    summary_idt sum_ID1;
+    summary_idt sum_ID2;
+    
+    {
+        smt_itpt *newitp1 = new smt_itpt();
+        //create cprover expression
+        symbol_exprt a("a", typet(ID_integer));
+        constant_exprt zero(ID_0, typet(ID_integer));
+        // a >= 0
+        binary_relation_exprt comp1(a, ID_equal, zero);
+        PTRef summary_temp1 = decider1->expression_to_ptref(comp1);
+        //manually set the PTRef
+        newitp1->setInterpolant(summary_temp1);
+        newitp1->setDecider(decider1);
+        newitp1->getTempl().addArg(decider1->expression_to_ptref(a));
+        newitp1->getTempl().setBody(summary_temp1);
+        
+        sum_ID1 = store1->insert_summary(newitp1, fun);
+        // insert_summary passes the ownership to summary_store, don't use *new_itp1 anymore
+        
+        char *s1 = decider1->getLogic()->printTerm(newitp1->getInterpolant());
+        std::cout << "Interpolant1 " << s1 << '\n';
+        
+    
+        smt_itpt *newitp2 = new smt_itpt();
+        //create another cprover expression
+        //symbol_exprt b("b", typet(ID_integer));
+        constant_exprt one(ID_1, typet(ID_integer));
+        // a >= 1
+        binary_relation_exprt comp2(a, ID_equal, one);
+        PTRef summary_temp2 = decider1->expression_to_ptref(comp2);
+        //manually set the PTRef
+        newitp2->setInterpolant(summary_temp2);
+        newitp2->setDecider(decider1);
+        newitp2->getTempl().addArg(decider1->expression_to_ptref(a));
+        newitp2->getTempl().setBody(summary_temp2);
+        
+        sum_ID2 = store1->insert_summary(newitp2, fun);
+        // insert_summary passes the ownership to summary_store, don't use *new_itp2 anymore
+        
+        char *s2 = decider1->getLogic()->printTerm(newitp2->getInterpolant());
+        std::cout << "Interpolant2  " << s2 << '\n';
+    }
+
+//serialization
+    const std::string& summary_file = "__summaries";
+    std::ofstream out;
+    out.open(summary_file.c_str());
+    store1->serialize(out);
+    out.close();
+    delete decider1;
+
+//create resources again (simulates the 2nd run)
+    smtcheck_opensmt2t* decider2 = new smtcheck_opensmt2t_uf(sop, "uf checker");
+    smt_summary_storet * store2 = new smt_summary_storet(decider2);
+    
+    std::vector <std::string> summary_files;
+    summary_files.push_back(summary_file);
+    
+    if (!summary_files.empty()) {
+        store2->deserialize(summary_files);
+    }
+    
+    itpt& itpOld1 = store1->find_summary(sum_ID1);
+    itpt& itpOld2 = store1->find_summary(sum_ID2);
+    
+    itpt& itpNew1 = store2->find_summary(sum_ID1);
+    itpt& itpNew2 = store2->find_summary(sum_ID2);
+    
+    auto res2 = itpNew2.equals(&itpOld2);
+    ASSERT_TRUE(res2);
+    
+    auto res1 = itpNew1.equals(&itpOld1);
+    ASSERT_TRUE(res1);   //why they have different PTRef?
+    
+    if(res1 && res2 ){
+        std::cout << "3rd test successfully passed!\n" <<'\n';
+    }
+    std::remove("__summaries");
+    delete decider2;
+}
+
+/********************************************************************
+ Test sumID preservation for two artificial summaries in LRA
+********************************************************************/
+TEST(test_IDequality, test_IDequality_serialize_deserialize_lra_twoSum) {
+    
+    solver_optionst sop;
+    smtcheck_opensmt2t_lra* decider1 = new smtcheck_opensmt2t_lra(sop, "lra checker");
+    smt_summary_storet * store1 = new smt_summary_storet(decider1);
+    
+    std::string fun{"beta"};
+    
+    summary_idt sum_ID1;
+    summary_idt sum_ID2;
+    
+    {
+        smt_itpt *newitp1 = new smt_itpt();
+        //create cprover expression
+        symbol_exprt a("a", typet(ID_real));
+        constant_exprt zero(ID_0, typet(ID_real));
+        // a >= 0
+        binary_relation_exprt comp1(a, ID_ge, zero);
+        PTRef summary_temp1 = decider1->expression_to_ptref(comp1);
+        //manually set the PTRef
+        newitp1->setInterpolant(summary_temp1);
+        newitp1->setDecider(decider1);
+        newitp1->getTempl().addArg(decider1->expression_to_ptref(a));
+        newitp1->getTempl().setBody(summary_temp1);
+        
+        sum_ID1 = store1->insert_summary(newitp1, fun);
+        // insert_summary passes the ownership to summary_store, don't use *new_itp anymore
+        
+        char *s1 = decider1->getLogic()->printTerm(newitp1->getInterpolant());
+        std::cout << "Interpolant1 " << s1 << '\n';
+        
+        
+        smt_itpt *newitp2 = new smt_itpt();
+        //create another cprover const expression
+        constant_exprt one(ID_1, typet(ID_real));
+        // a >= 1
+        binary_relation_exprt comp2(a, ID_ge, one);
+        PTRef summary_temp2 = decider1->expression_to_ptref(comp2);
+        //manually set the PTRef
+        newitp2->setInterpolant(summary_temp2);
+        newitp2->setDecider(decider1);
+        newitp2->getTempl().addArg(decider1->expression_to_ptref(a));
+        newitp2->getTempl().setBody(summary_temp2);
+        
+        sum_ID2 = store1->insert_summary(newitp2, fun);
+        
+        char *s2 = decider1->getLogic()->printTerm(newitp2->getInterpolant());
+        std::cout << "Interpolant2  " << s2 << '\n';
+    }
+
+//serialization
+    const std::string& summary_file = "__summaries";
+    std::ofstream out;
+    out.open(summary_file.c_str());
+    store1->serialize(out);
+    out.close();
+    delete decider1;
+
+//create resources again (simulates the 2nd run)
+    smtcheck_opensmt2t* decider2 = new smtcheck_opensmt2t_lra(sop, "lra checker");
+    smt_summary_storet * store2 = new smt_summary_storet(decider2);
+    
+    std::vector <std::string> summary_files;
+    summary_files.push_back(summary_file);
+    
+    if (!summary_files.empty()) {
+        store2->deserialize(summary_files);
+    }
+    
+    itpt& itpOld1 = store1->find_summary(sum_ID1);
+    itpt& itpOld2 = store1->find_summary(sum_ID2);
+    
+    itpt& itpNew1 = store2->find_summary(sum_ID1);
+    itpt& itpNew2 = store2->find_summary(sum_ID2);
+//check if the associated itp to tehe ID are equal
+    
+    auto res2 = itpNew2.equals(&itpOld2);
+    ASSERT_TRUE(res2);
+    
+    auto res1 = itpNew1.equals(&itpOld1);
+    ASSERT_TRUE(res1);
+    
+    if(res1 && res2 ){
+        std::cout << "4th test successfully passed!\n" <<'\n';
+    }
+    std::remove("__summaries");
+}
