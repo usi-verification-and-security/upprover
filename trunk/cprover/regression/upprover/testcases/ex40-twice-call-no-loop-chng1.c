138aa2e5ff09a1b0091fd31381e625129aeab8d0
Sepideh Asadi <asadis@usi.ch>
2020-10-06 15:38:04 +0200

    add more regression test benchmarks for upprover

---
+++ trunk/cprover/regression/upprover/testcases/ex40-twice-call-no-loop-chng1.c
@@ -0,0 +1,38 @@
+unsigned int nondetUInt();
+
+int sum (int w)
+{
+    int s=0;
+    
+    unsigned n = nondetUInt();
+    __CPROVER_assume(n>0);
+    __CPROVER_assume(n<10);
+
+    for (int i = 0; i <2; i++)
+    {
+
+   	 s = s+n+w+1;
+    }
+    return s;
+}
+
+int main()
+{
+    int a;
+    __CPROVER_assume(a>0);
+    __CPROVER_assume(a<30);
+    // for (int i = 0; i < 2; ++i)
+    // {
+      a=sum(a);
+      a++;
+      assert(a>=0);
+
+      a=sum(a);
+      a++;
+      assert(a>=0);
+    // }
+}
+
+
+//omega FUNCTIONS size is : 6
+//summary for 2 sum#0 and sum#1 
